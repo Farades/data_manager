efa29213223fd501dd289ef0adf51e1cbfb82f4d
Matsepura A <creg2007@gmail.com>
2015-04-29 16:34:57 +0300

    com name from command line args

--- src/ru/entel/Main.java
+++ src/ru/entel/Main.java
@@ -15,6 +15,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String temp = "";
         ModbusMaster mbm = null;
+        String comName = null;
+        if (args.length == 0) {
+            System.out.println("Введите название COM-порта");
+            System.exit(1);
+        } else {
+            comName = args[0];
+        }
         while(true) {
             try {
                 temp = br.readLine();
@@ -27,12 +34,12 @@ public class Main {
                 mbm.running = false;
             }
             else if (temp.equals("go")) {
-                mbm = new ModbusMaster("COM2", 19200, 8, "none", 1, "rtu", false, 200);
+                mbm = new ModbusMaster(comName, 19200, 8, "none", 1, "rtu", false, 100);
 
                 ModbusMasterSlave slave_1 = new ModbusMasterSlave(1, "V1");
                 try {
-                    slave_1.addChannel(new ModbusSlaveChannel("INPUT", 10, 4, ModbusFunction.READ_INPUT_REGS_4, ModbusRegType.FLOAT32, 50));
-                    //slave_1.addChannel(new ModbusSlaveChannel("Holding", 4, 4, ModbusFunction.READ_HOLDING_REGS_3 ,ModbusRegType.FLOAT32, 50));
+                    slave_1.addChannel(new ModbusSlaveChannel("DIP Switch", 0, 6, ModbusFunction.READ_COIL_REGS_1, ModbusRegType.BIT, 50));
+                    slave_1.addChannel(new ModbusSlaveChannel("Potentiometr's", 1, 3, ModbusFunction.READ_HOLDING_REGS_3 ,ModbusRegType.INT16, 50));
                 } catch (ModbusChannelAlreadyExistException ex) {
                     System.out.println(ex.getMessage());
                     System.exit(1);
@@ -41,17 +48,6 @@ public class Main {
                     System.exit(1);
                 }
                 mbm.addModbusSlave(slave_1);
-
-                ModbusMasterSlave slave_2 = new ModbusMasterSlave(5, "Amp");
-                try {
-                    //slave_2.addChannel(new ModbusSlaveChannel("COIL", 10, 2, ModbusFunction.READ_COIL_REGS_1, ModbusRegType.BIT, 50));
-                    slave_2.addChannel(new ModbusSlaveChannel("DISCRETE", 5, 2, ModbusFunction.READ_DISCRETE_INPUT_2, ModbusRegType.BIT, 50));
-                } catch (ModbusChannelAlreadyExistException ex) {
-                    System.out.println("Repeated slave channel.");
-                } catch (ModbusOddQuantityRegException ex) {
-                    System.out.println(ex.getMessage());
-                }
-                mbm.addModbusSlave(slave_2);
 
                 new Thread(mbm).start();
             } else {
